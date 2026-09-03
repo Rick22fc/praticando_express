49223f3b2abe18279c6aa3287e4b40a637aa9e68
Rick22fc <henrique_c_martins@estudante.sesisenai.org.br>
2026-09-03 17:12:13 -0300

    feat

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { pool } from './database/db.js'
 
 const app = express()
 const port = 3000
@@ -9,6 +10,15 @@ app.get("/",(req,res) => {
     res.json("Hello World")
 })
 
+app.get("/frutas", async (req, res) =>{
+    try{
+        const response = await pool.query("SELECT * FROM frutas");
+        res.json(response.rows);
+    } catch(error){
+        console.log.error(error);
+    }
+});
+
 app.listen(port, () =>{
-    console.log(`Seja bem vindo a minha APIhttp://localhost:${port}`);
-})
+    console.log(`Seja bem vindo a minha API http://localhost:${port}`);
+});
